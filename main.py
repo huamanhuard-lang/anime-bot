393cb28df68929dee29cef6ac3feb14dad0a918b
huamanhuard-lang <huamanhuard@gmail.com>
2026-06-04 10:10:00 +0300

    Update1 main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-TOKEN = os.getenv("TOKEN")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Anime bot çalışıyor 🔥")
-
-app = ApplicationBuilder().token(TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+print("bot basladi")
