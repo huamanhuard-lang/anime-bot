f66940a60491ab28a778b64f891b2e9833defbd1
huamanhuard-lang <huamanhuard@gmail.com>
2026-06-04 09:56:47 +0300

    add bot code

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = "BURAYA_DOKUNMA"  # Render bunu kullanmayacak
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Anime bot çalışıyor 🔥")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
