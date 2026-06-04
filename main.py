b809c9805bebbba47515c3c36b111f25c5dfb7fa
huamanhuard-lang <huamanhuard@gmail.com>
2026-06-04 10:00:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "BURAYA_DOKUNMA"  # Render bunu kullanmayacak
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Anime bot çalışıyor 🔥")
